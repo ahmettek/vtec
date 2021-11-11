5ced8299c67a757188fff5bd68a858824d55e6ac
Ahmet Tek <atek@THB-ARG-44.local>
2021-11-11 23:41:58 +0300

    add gopi

--- main.go
+++ main.go
@@ -1,22 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "html"
+    "encoding/json"
     "log"
     "net/http"
 )
-
+type user struct {
+    ID   string `json:"id"`
+    Name string `json:"name"`
+}
 func main() {
 
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusCreated)
+        json.NewEncoder(w).Encode(user{ID: "ahmet",Name: "Tek"})
     })
 
     http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
+        w.Header().Set("Content-Type", "application/json")
+        w.WriteHeader(http.StatusCreated)
+        json.NewEncoder(w).Encode(user{ID: "ahmet",Name: "Tek"})
     })
+    mux := http.NewServeMux()
+    mux.Handle("/api/keys",&basicApiHandler{
 
-    log.Fatal(http.ListenAndServe(":8081", nil))
+    })
+    log.Fatal(http.ListenAndServe(":8081", mux))
+}
 
+type basicApiHandler struct {}
+
+func (h *basicApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("content-type", "application/json")
+    switch {
+    case r.Method == http.MethodGet:
+        h.Get(w, r)
+        return
+    case r.Method == http.MethodPut:
+        h.Set(w, r)
+        return
+    default:
+        notFound(w, r)
+        return
+    }
+}
+
+func (h *basicApiHandler) Get(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(user{ID: "ahmet",Name: "Tek"})
+}
+
+func (h *basicApiHandler) Set(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(user{ID: "ahmet",Name: "Tek"})
+}
+func notFound(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte("not found"))
 }
---
+++ pkg/api/gopi.go
@@ -0,0 +1,73 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Gopi struct {
+	routes  []Route
+	mux *http.ServeMux
+}
+
+// New returns an initialized Gopi structure, ready to use.
+func New() *Gopi {
+
+	mux := http.NewServeMux()
+
+	return &Gopi{
+		mux: mux,
+	}
+}
+
+func Serve(g*Gopi) {
+	g.mux.Handle("/api/keys",&basicApiHandler{})
+}
+
+func (e *Gopi) GET(path string,handler func(w http.ResponseWriter, r *http.Request)) {
+	 e.Add(http.MethodGet,path,handler)
+}
+
+func (e *Gopi) Add(method string,path string, handler func(w http.ResponseWriter, r *http.Request)) {
+	routes := append(e.routes, Route{Method: method, Path: path,Handler: handler})
+	e.routes=routes;
+}
+
+type Route struct {
+Method string `json:"method"`
+Path   string `json:"path"`
+Handler   func(w http.ResponseWriter, r *http.Request) `json:"name"`
+}
+
+type basicApiHandler struct {}
+
+func (h *basicApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	switch {
+	case r.Method == http.MethodGet:
+		h.Get(w, r)
+		return
+	case r.Method == http.MethodPut:
+		h.Set(w, r)
+		return
+	default:
+		notFound(w, r)
+		return
+	}
+}
+
+func (h *basicApiHandler) Get(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(true)
+}
+
+func (h *basicApiHandler) Set(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(true)
+}
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("not found"))
+}
