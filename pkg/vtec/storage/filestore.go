2eaf311a591eb664ed4cda4916970d461a00fe78
Ahmet Tek <atek@THB-ARG-44.local>
2021-11-13 00:38:25 +0300

    file storage

--- pkg/vtec/storage/filestore.go
+++ pkg/vtec/storage/filestore.go
@@ -32,7 +32,7 @@ func (fs*FileStore) Write(data map[string]string) error {
 		return err
 	}
 	defer f.Close()
-	if strings.HasSuffix(fs.Dir, ".gz") {
+	if strings.HasSuffix(fs.Dir, "") {
 		w := gzip.NewWriter(f)
 		defer w.Close()
 		enc := json.NewEncoder(w)
